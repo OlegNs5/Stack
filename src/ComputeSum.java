c15530b1fbdcef00c39fa70ff0039351533af544
Windows 10 <irobatic28@gmail.com>
2022-08-09 06:34:19 +0300

    comit_1.1 debugger

---
+++ src/ComputeSum.java
@@ -0,0 +1,11 @@
+
+public class ComputeSum {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        Sum sum = new Sum();
+        System.out.println("Suma este: "+ sum.sumOfNumbers(a,b));
+//        System.out.println("Suma este: "+ sum.sumOfNumbers(a,b));
+    }
+}
---
+++ src/Sum.java
@@ -0,0 +1,9 @@
+public class Sum {
+
+    public int sumOfNumbers(int a, int b){
+        int sum = 0;
+        sum = a+b;
+//        return sum;
+        return sum;
+    }
+}
